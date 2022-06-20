1b18ce9becf85ad2a88f734000b38fe92693e622
Benceeyang <byang@icstars.org>
2022-06-20 12:59:12 -0500

    Server connection

---
+++ prototype/dbFiles/dbConfig.js
@@ -0,0 +1,16 @@
+// const config = {
+//     authentication: {
+//       options: {
+//         userName: "c10admin", // update me
+//         password: "#Icstars123" // update me
+//       },
+//       type: "default"
+//     },
+//     server: "c10.database.windows.net", // update me
+//     options: {
+//       database: "COURTHOUSE", //update me
+//       encrypt: true
+//     }
+//   };
+
+// module.exports = config;
